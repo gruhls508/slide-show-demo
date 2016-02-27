2d00e16cf4243152e8e897ee2d98fd5a5a7fec3d
Glen Ruhl <intradinedev@gmail.com>
2016-02-27 15:36:35 -0500

    Initial Commit

---
+++ SlideShowDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideShowDemo
+//
+//  Created by Glen Ruhl on 2/27/16.
+//  Copyright © 2016 intradine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
